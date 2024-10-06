5e02b18cc5eb725c7f707b1916a489098fbf9a01
Adora <adora@youshangyudeMacBook-Air.local>
2024-10-06 15:13:33 +0800

    example(templateView)

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('<str:arg>/', views.HomeView.as_view(), name='index'),
+    path('example/<str:arg>/', views.example_view, name='example_name'),
+    path('example2/<str:arg>/', views.example_view_2, name='example_name_2'),
+    path('example3/<str:my_arg>/', views.example_view_3, name='example_name_3'),
+    path('example4/<str:arg>/', views.example_view_4, name='example_name_4'),
+    path('example5/<str:arg>/', views.example_view_5, name='example_name_5'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,44 @@
-from django.template.response import TemplateResponse
-from django.shortcuts import render
-from datetime import datetime
+from datetime import date
+from django.views.generic import TemplateView
 
-def index(request):
-    return render(request, 'blog/index.html')
+from django.template.response import TemplateResponse
+
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+
+def example_view(request, arg, **kwargs):
+    kwargs['today'] = date.today()
+    return TemplateResponse(request, 'blog/example.html', { 'arg': arg, **kwargs })
+
+def example_view_2(request, arg, **kwargs):
+    kwargs['today'] = date.today()
+    return HttpResponse(f'example 2 {arg}, today is { kwargs["today"] }')
+
+def example_view_3(request, my_arg, **kwargs):
+    kwargs['today'] = date.today()
+    template = loader.get_template('blog/example3.html')
+    context = {
+        'my_arg': my_arg,
+        **kwargs
+    }
+    return HttpResponse(template.render(context, request))
+
+def example_view_4(request, arg, **kwargs):
+    kwargs['today'] = date.today()
+    return render(request, 'blog/example4.html', {'arg': arg, **kwargs})
+
+def example_view_5(request, arg, **kwargs):
+    kwargs['today'] = date.today()
+    return TemplateResponse(request, 'example5.html', {'arg': arg, **kwargs})
+
+class HomeView(TemplateView):
+    template_name = "blog/index.html"
+
+    def get_context_data(self, arg, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['today'] = date.today()
+        context['arg'] = arg
+        return context
+    
